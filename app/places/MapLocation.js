3b5d3a2ceb517fb1a29b97fb61252ff1a6629372
olegs7771 <olegs7771@gmail.com>
2019-12-02 16:42:37 +0200

    ok

--- app/places/MapLocation.js
+++ app/places/MapLocation.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, Dimensions} from 'react-native';
 import MapView from 'react-native-maps';
 
 class MapLocation extends Component {
+  state = {
+    focusedRegion: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  };
+
   render() {
     return (
       <View style={styles.container}>
--- app/places/SelectPlace.js
+++ app/places/SelectPlace.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Dimensions} from 'react-native';
 import TextForm from '../components/TextForm';
 import MapLocation from './MapLocation';
 
 import {connect} from 'react-redux';
 
 export class SelectPlace extends Component {
-  state = {
-    placeName: '',
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeName: '',
+      isLandScape: false,
+    };
+    Dimensions.addEventListener('change', dims => {
+      console.log('dims.window.width', dims.window.width);
+      console.log('dims.window.height', dims.window.height);
+      if (dims.window.width > dims.window.height) {
+        this.setState({
+          isLandScape: true,
+        });
+      }
+    });
+  }
   _sharePlace = e => {
     const newPlace = {
       name: this.state.placeName,
@@ -17,22 +30,24 @@ export class SelectPlace extends Component {
   };
 
   render() {
-    console.log('this.state', this.state);
+    console.log('isLandScape', this.state.isLandScape);
 
     return (
       <View style={styles.container}>
         <Text style={styles.textTitle}> Here Select Places </Text>
-        <View style={styles.containerForm}>
-          <TextForm
-            type="text"
-            onChangeText={text => this.setState({placeName: text})}
-            placeName={this.state.placeName}
-            value={this.state.placeName}
-            placeholder="Pick the Name"
-          />
-        </View>
-        <View>
-          <Text>Choose Picture Here</Text>
+        <View style={styles.container_Name_Picture}>
+          <View style={styles.containerForm}>
+            <TextForm
+              type="text"
+              onChangeText={text => this.setState({placeName: text})}
+              placeName={this.state.placeName}
+              value={this.state.placeName}
+              placeholder="Pick the Name"
+            />
+          </View>
+          <View style={styles.containerPicture}>
+            <Text>Choose Picture Here</Text>
+          </View>
         </View>
         <View style={styles.containerMap}>
           <View style={styles.containerText}>
@@ -61,9 +76,21 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     alignItems: 'center',
   },
-  containerForm: {
+  containerFormLandScape: {
     borderWidth: 1,
-    width: '60%',
+    width: '40%',
+  },
+  containerFormPortrait: {
+    borderWidth: 1,
+    width: '80%',
+  },
+  containerPictureLandScape: {
+    width: '40%',
+    borderWidth: 1,
+  },
+  containerPicturePortrait: {
+    width: '80%',
+    borderWidth: 1,
   },
   containerMap: {
     width: '80%',
@@ -74,6 +101,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
 
     paddingVertical: 5,
+  },
+  container_Name_Picture_LandScape: {
+    width: '100%',
+    flexDirection: 'row',
+  },
+  container_Name_Picture_Portrait: {
+    width: '100%',
+    flexDirection: 'column',
   },
   textTitle: {
     fontSize: 20,
