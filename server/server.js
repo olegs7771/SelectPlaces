840a7de71ee9a1d5ab3eed8b4f7a14622c6b45c3
Oleg Smushkevich <olegs777@bezeqint.net>
2019-12-21 22:01:32 +0200

    ok

--- server/server.js
+++ server/server.js
@@ -44,3 +44,13 @@ app.use(fileUpload());
 app.use('/', index);
 app.use('/api', places);
 app.use('/api', auth);
+
+//Exercises
+
+const obj = {x: 5, y: 20, z: 3};
+
+const mult = ({x, y, z}) => {
+  return x * y * z;
+};
+
+console.log(mult(obj));
